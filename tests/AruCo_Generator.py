83721ff82691f0dee0b31caa9d6b3a092344d2ed
Mohamed Taher Othmen <mohamedtaher.othman@gmail.com>
2026-04-01 12:05:17 +0100

    Aruco Pdf Gen

--- tests/AruCo_Generator.py
+++ tests/AruCo_Generator.py
@@ -2,20 +2,15 @@ import cv2
 import os
 import numpy as np
 
+def arucoDictInit():
+    return cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
+
+
 def arucoGenerator(id, size=200):
-    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_50)
+    arucoDict = arucoDictInit()
     marker_image = np.zeros((size, size), dtype=np.uint8)
     cv2.aruco.generateImageMarker(arucoDict, id=id, sidePixels=size, img=marker_image)
-    return marker_image  
+    return marker_image 
 
 def saveImg(imageMat, id, path="../exports"):
     cv2.imwrite(os.path.join(path, f"Aruco_{id}.png")  , imageMat)
-
-aruco1 = arucoGenerator(0, 200)
-saveImg(aruco1, 0)
-aruco2 = arucoGenerator(1, 200)
-saveImg(aruco2, 1)
-aruco3 = arucoGenerator(2, 200)
-saveImg(aruco3, 2)
-aruco4 = arucoGenerator(3, 200)
-saveImg(aruco4, 3)
